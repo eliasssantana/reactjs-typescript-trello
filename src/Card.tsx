1fa98bc65a9925e8bc090413c7a3698d2a3bb376
Elias Santana <eliassilva0045@gmail.com>
2021-11-27 18:56:35 -0300

    feat(card) show add button

---
+++ src/Card.tsx
@@ -0,0 +1,9 @@
+import { CardContainer } from "./styles"
+interface CardProps { 
+    text: string 
+} 
+const Card = ({ text }: CardProps) => { 
+    return <CardContainer>{text}</CardContainer> 
+}
+
+export default Card;
